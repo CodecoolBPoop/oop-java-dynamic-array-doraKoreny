508be04092bd543df1acedab95e10d8452bed4e0
doraKoreny <dorakoreny@gmail.com>
2018-12-05 18:02:00 +0100

    add(), insert() are implemented & passed the test, remove() haven't passed yet

--- src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
+++ src/main/java/com/codecool/dynamicArrayDojo/DynamicIntArray.java
@@ -1,9 +1,78 @@
 package com.codecool.dynamicArrayDojo;
 
+import java.util.Arrays;
+
 // put your code here!
 public class DynamicIntArray {
+
+    private int lengthOfIntArray = 0;
+
+    private int intArray[] = new int[lengthOfIntArray];
+
+    public int getLengthOfIntArray() {
+        return lengthOfIntArray;
+    }
+
+    public void setLengthOfIntArray(int lengthOfIntArray) {
+        this.lengthOfIntArray = lengthOfIntArray;
+    }
+
+    public int[] getIntArray() {
+        return intArray;
+    }
+
+    public void setIntArray(int[] intArray) {
+        this.intArray = intArray;
+    }
+
+    public void add(int numberToAdd) {
+        int[] extendedArray = new int[intArray.length + 1];
+        System.arraycopy(intArray, 0, extendedArray, 0, intArray.length);
+        extendedArray[extendedArray.length-1] = numberToAdd;
+        intArray = extendedArray;
+    }
+
+    public void remove(int indexToRemove) {
+        if (indexToRemove <= intArray.length) {
+            int[] decreasedArray = new int[intArray.length - 1];
+            System.arraycopy(intArray,0,decreasedArray,0,indexToRemove - 1);
+            System.arraycopy(intArray,indexToRemove + 1,decreasedArray, indexToRemove, intArray.length - indexToRemove);
+            //System.out.println(Arrays.toString(decreasedArray));
+
+            intArray = decreasedArray;
+        }
+
+
+    }
+
+    public void insert(int index, int numberToInsert) {
+        int[] insertedArray = new int[intArray.length + 1];
+        if (index <= intArray.length) {
+            System.arraycopy(intArray, 0, insertedArray, 0,index );
+            insertedArray[index] = numberToInsert;
+            System.arraycopy(intArray,index , insertedArray, index + 1, intArray.length - index);
+            intArray = insertedArray;
+        } else {
+            add(numberToInsert);
+        }
+
+
+    }
+
     @Override
     public String toString() {
-        return "Implement me plz.. Pretty plz..";
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < intArray.length; i++) {
+            result.append(" ");
+            result.append(String.valueOf(intArray[i]));
+        }
+        return result.toString();
     }
+
+    public DynamicIntArray(int arrayLength) {
+        setLengthOfIntArray(arrayLength);
+    }
+
+    public DynamicIntArray() {}
+
 }
